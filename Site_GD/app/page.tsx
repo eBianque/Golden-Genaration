53d43ef168574fa46e72b7cfaa8a72acff0dc2ee
eBianque <eduardobianque@hotmail.com>
2026-05-29 01:29:14 -0300

    git

--- Site_GD/app/page.tsx
+++ Site_GD/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           href="/gm-page"
           className="bg-yellow-500 text-blue-900 px-6 py-3 rounded-lg font-semibold hover:bg-yellow-100 transition text-center hover:bg-yellow-100 hover:scale-105 transition"
         >
-          Mestragem
+          Escudo do Mestre
         </Link>
       </nav>
     </main>
